c2ebb6c639cd5bcc92a195a301cf1977b1bec32e
Tom Smith <tom@craftscale.uk>
2025-01-30 14:41:53 +0000

    Updated models.py with Blog and User model

--- myApp/models.py
+++ myApp/models.py
@@ -1,3 +1,22 @@
+from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
+
 from django.db import models
 
 # Create your models here.
+class User(AbstractUser):
+  email = models.EmailField(max_length=200, unique=True)
+  password = models.CharField(max_length=100)
+  date_created = models.DateTimeField(default=timezone.now)
+
+  def __str__(self):
+    return self.email
+  
+class Blog(models.Model):
+  title = models.CharField(max_length=200)
+  content = models.TextField()
+  author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blogs")
+  date_created = models.DateTimeField(default=timezone.now)
+
+  def __str__(self):
+    return f"{self.title} by {self.author}"
